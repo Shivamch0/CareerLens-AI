7a042422eef894499f177f2d16144dd9df89cb8b
Shivam <shivamch0865@gmail.com>
2026-04-07 21:11:42 +0530

    Navabar middle part link styling completed

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
-function Button({title , className}) {
+function Button({title , className , onClick}) {
   return (
-    <button className={` bg-blue-500 py-1 px-3 rounded-full cursor-pointer hover:bg-blue-800 transition-colors duration-300`}>
+    <button 
+    className={` bg-blue-500 py-1 px-3 rounded-full cursor-pointer hover:bg-blue-800 transition-colors duration-300`} 
+    onClick={onClick} 
+    >
       {title}
     </button>
   )
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,33 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Button from './Button'
-import logo from "../assets/logo.png"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "./Button";
+import logo from "../assets/logo.png";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
   return (
-    <header className='  mt-4 p-2'>
-      <nav className='flex items-center justify-between'>
-
-        <div className='flex items-center '>
-          <img className='w-12 h-12 rounded-full' src={logo} alt="" /><h3 className='text-2xl font-bold text-blue-600'>CAREER</h3><p className='text-2xl font-bold '>LENS</p>
+    <header className="  mt-4 p-2">
+      <nav className="flex items-center justify-between">
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={handleNavigate}
+        >
+          <img className="w-12 h-12 rounded-full" src={logo} alt="" />
+          <h3 className="text-2xl font-bold text-blue-600">CAREER</h3>
+          <p className="text-2xl font-bold ">LENS</p>
+        </div>
+        <div className="flex gap-10 bg-white/10 border-white/20 py-1 px-4 rounded-full text-sm text-gray-400 ">
+          <Link
+            to={"/learning"}
+            className="relative py-2 px-3 transform hover:text-gray-200  duration-300 hover:text-lg"
+          >
+            Learning
+          </Link>
+          <Link
+            to={"/courses"}
+            className=" py-2 px-3 transform hover:text-gray-200 duration-300 hover:text-lg"
+          >
+            Courses
+          </Link>
+          <Link
+            to={"/skills"}
+            className=" py-2 px-3 transform hover:text-gray-200  duration-300 hover:text-lg"
+          >
+            Skills
+          </Link>
+          <Link
+            to={"/interview"}
+            className=" py-2 px-3 transform hover:text-gray-200  duration-300 hover:text-lg"
+          >
+            Interview
+          </Link>
+          <Link
+            to={"/resume"}
+            className=" py-2 px-3 transform hover:text-gray-200  duration-300 hover:text-lg"
+          >
+            Resume
+          </Link>
         </div>
 
-        <div className='flex gap-10 bg-white/10 border-white/20 py-1 px-4 rounded-full text-sm'>
-          <Link to={'/learning'} className='py-2 px-3'>Learning</Link>
-          <Link to={'/courses'} className=' py-2 px-3'>Courses</Link>
-          <Link to={'/skills'} className=' py-2 px-3'>Skills</Link>
-          <Link to={'/interview'} className=' py-2 px-3'>Interview</Link>
-          <Link to={'/resume'} className=' py-2 px-3'>Resume</Link>
-        </div>
-
-        <div className='flex gap-5' >
-          <Button title="Get Started" />
+        <div className="flex gap-5">
+          <Button title="Get Started" onClick={() => navigate("/login")} />
           <button>Toggle</button>
         </div>
-
       </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
