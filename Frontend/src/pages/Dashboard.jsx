52715c63fedb37cdb205e999efdcdeab9a92b0ef
Shivam <shivamch0865@gmail.com>
2026-04-09 18:58:23 +0530

    Dashboard layout ready

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+// Hooks Imports
+import { useTheme } from "../Provider/ThemeProvider"
+
+// Components Imports
+import StatsCard from "../components/StatsCard"
+import ActivityCard from "../components/ActivityCard"
+import SuggestionCard from "../components/SuggestionCard"
+
+// Other Imports
 
 function Dashboard() {
+  const { isDark } = useTheme()
   return (
-    <div>Dashboard</div>
+    <section>
+      <h3>Welcome,Shivam👋</h3>
+      <p>B.Tech CSE | 4th Year</p>
+
+      <div>
+
+        <p>Skils: 5 | Test: 2 | Match Score: 78%</p>
+
+        <div>
+          <StatsCard />
+          <StatsCard />
+          <StatsCard />
+        </div>
+
+        <div>
+          <ActivityCard />
+          <div>
+            <SuggestionCard />
+            <SuggestionCard />
+            <SuggestionCard />
+          </div>
+        </div>
+
+        <div>
+          <ActivityCard />
+          <ActivityCard />
+        </div>
+
+      </div>
+    </section>
   )
 }
 
