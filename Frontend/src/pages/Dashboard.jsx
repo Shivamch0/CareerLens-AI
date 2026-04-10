679e72dd495ec4b43f57a8d193d3c4856aecf4f2
Shivam <shivamch0865@gmail.com>
2026-04-10 14:04:06 +0530

    Images Import at Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,13 @@ import ActivityCard from "../components/ActivityCard"
 import SuggestionCard from "../components/SuggestionCard"
 
 // Other Imports
+import blueIcon from "../assets/blue_icon.png"
+import clipboard from "../assets/clipboard.png"
+import greenIcon from "../assets/green_icon.png"
+import micIcon from "../assets/mic-icon.png"
+import notes from "../assets/notes.png"
+import redIcon from "../assets/red_icon.png"
+import youngMan from "../assets/young_man.png"
 
 function Dashboard() {
   const { isDark } = useTheme()
