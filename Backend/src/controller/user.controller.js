b7669df581642a76a0e66a263b3fee83bf3bca44
Shivam <shivamch0865@gmail.com>
2026-04-11 11:53:17 +0530

    created the user controller structure

---
+++ Backend/src/controller/user.controller.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model.js"
+
+// Utils Imports
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+
+const registerUser = asyncHandler ( async (req , res) => {
+
+});
+
+const loginUser = asyncHandler ( async (req , res) => {
+
+});
+
+const logoutUser = asyncHandler ( async (req , res) => {
+
+});
+
+const getCurrentUser = asyncHandler ( async (req , res) => {
+
+});
+
+const refreshToken = asyncHandler ( async (req , res) => {
+
+});
+
+export { registerUser , loginUser , logoutUser , getCurrentUser , refreshToken}
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -32,6 +32,9 @@ const userSchema = new mongoose.Schema({
         branch : String,
         marks : String
     },
+    refreshToken : {
+        type : String
+    },
     
 } , { timestamps : true});
 
