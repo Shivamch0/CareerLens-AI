aa07cc255f6080c401c132bdeddc9e715fe9568b
Shivam <shivamch0865@gmail.com>
2026-04-11 11:48:19 +0530

    User model created

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true , 
+        trim : true
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    avatar : {
+        type : String
+    },
+    interests : {
+        type : [String]
+    },
+    skills : {
+        type : [String]
+    },
+    education : {
+        degree : String,
+        branch : String,
+        marks : String
+    },
+    
+} , { timestamps : true});
+
+export const User = mongoose.model("User" , userSchema)
