1f98a35de18258c2510e881a6ab2125dd984047c
Shivam <shivamch0865@gmail.com>
2026-04-11 12:34:58 +0530

    register user controller created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express();
+
+app.set("trust proxy", 1);
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use("/uploads" , express.static("uploads"))
+
+app.use(cookieParser());
 
 export default app;
--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -8,6 +8,49 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler ( async (req , res) => {
     const {userName , email , password} = req.body;
+    if(!userName || !email || !password ){
+        throw new ApiError(400 , 'Fill all the fields...')
+    }
+
+    const existedUser = await User.findOne({email});
+    if(existedUser){
+        throw new ApiError(400 , 'User already registered with this email...')
+    }
+
+    const user = await User.create({
+        userName,
+        email : email.toLowerCase(),
+        password
+    })
+
+    const accessToken = await User.generateAccessToken()
+    const refreshToken = await User.generateRefreshToken()
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave : false});
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createdUser){
+        throw new ApiError(400 , 'Something went wrong while registering user')
+    }
+
+    const options = {
+        httpOnly : true,
+        secure : process.env.NODE_ENV === 'production',
+        smaeSite :"None" ,
+        path : "/",
+        maxAge : "7 * 24 * 60 * 60 * 1000"
+    }
+
+    return res.status(200)
+            .cookie("AccessToken" , options , accessToken)
+            .cookie('RefreshToken' , options , refreshToken)
+            .json(
+                new ApiResponse(200 , createdUser , "User Created Successfully")
+            )
+
 });
 
 const loginUser = asyncHandler ( async (req , res) => {
