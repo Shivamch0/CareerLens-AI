0b78fd6241d53641d0870746f9d3d14a64fbc226
Shivam <shivamch0865@gmail.com>
2026-04-16 10:42:56 +0530

    Aptitude model created

--- Backend/src/models/apptitude.model.js
+++ Backend/src/models/apptitude.model.js
@@ -1,7 +1,25 @@
 import mongoose from "mongoose"
 
-const apptitudeSchema = new mongoose.model({
-
+const aptitudeSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    score : {
+        type : Number
+    },
+    totalQuestions : {
+        type : Number
+    },
+    answer : {
+        type : [{
+            questuionId : String,
+            selectedOptions : String,
+            isCorrect : Boolean
+        }]
+    },
+    
 } , {timeStamps : true})
 
-export const Apptitude = mongoose.model("Apptitude" , apptitudeSchema)
+export const Aptitude = mongoose.model("Aptitude" , aptitudeSchema)
