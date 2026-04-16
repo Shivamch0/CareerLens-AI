81f086b0763493665c74a9113f19c4cfd0dc7911
Shivam <shivamch0865@gmail.com>
2026-04-16 10:47:55 +0530

    interview model created

--- Backend/src/models/interview.model.js
+++ Backend/src/models/interview.model.js
@@ -1,7 +1,29 @@
 import mongoose from 'mongoose'
 
 const interviewSchema = new mongoose.Schema({
-
-})
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    questionAnswer : {
+        type : [
+            {
+                question : String,
+                answer : String,
+                score : Number
+            }
+        ]
+    },
+    individualQuestionScore : {
+        type : []
+    },
+    feedback : {
+        type : String
+    },
+    OverallScore : {
+        type : Number
+    }
+} , {timestamps : true})
 
 export const Interview = mongoose.model('Interview' , interviewSchema);
