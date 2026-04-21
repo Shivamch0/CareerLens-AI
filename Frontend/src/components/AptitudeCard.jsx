25f5ecb9bf6eac808045a2bccc61e6d344241c9a
Shivam <choudharyshivam0508@gmail.com>
2026-04-21 11:10:19 +0530

    apptitude card completed

--- Frontend/src/components/AptitudeCard.jsx
+++ Frontend/src/components/AptitudeCard.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { useTheme } from "../Provider/ThemeProvider";
 
-const AptitudeCard = () => {
+const AptitudeCard = ({ image, title, para, question, minute, progress = 40 , style , bgColor , progressColor}) => {
+  const { isDark } = useTheme();
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className={` w-[280px] rounded-2xl  shadow-sm p-6 flex flex-col items-center text-center cursor-pointer transition hover:shadow-md border ${style ? style  : ""} `}>
 
-export default AptitudeCard
+      <div className={`w-30 h-30 rounded-full flex items-center justify-center mb-4 ${bgColor}`}>
+        <img
+          src={image}
+          alt="card-icon"
+          className="mt-3"
+        />
+      </div>
+
+      <h3 className={`text-lg font-semibold ${isDark ? "text-white" : "text-gray-800"} `}>
+        {title}
+      </h3>
+
+      <p className={`text-sm text-gray-500 mt-2 leading-relaxed`}>
+        {para}
+      </p>
+
+      <div className={`flex gap-2 mt-4 text-sm font-medium ${isDark ? "text-white" : "text-gray-600"}`}>
+        <span>{question} Questions</span>
+        <span>|</span>
+        <span>{minute} Min</span>
+      </div>
+
+      <div className="w-full h-2 bg-gray-200 rounded-full mt-4 overflow-hidden">
+        <div
+          className={`h-full ${progressColor} rounded-full`}
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default AptitudeCard;
--- Frontend/src/pages/Apptitude/Assessment.jsx
+++ Frontend/src/pages/Apptitude/Assessment.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+// Hooks Imports
+import { useTheme } from "../../Provider/ThemeProvider"
+
+// Components Imports
+import AptitudeCard from "../../components/AptitudeCard"
+
+// Other Imports
+import brain from "../../assets/brain.png"
+import greenHeart from "../../assets/greenHeart.png"
 
 const Assessment = () => {
+  const { isDark } = useTheme()
   return (
-    <div>Assessment</div>
+    <section className={`text-center ${isDark ? "text-white" : ""}`}>
+      <h2 className={`text-4xl font-bold mb-2`}>Assessment Overview</h2>
+      <p className="text-base text-gray-500 mb-2">The test is divided into 2 sections</p>
+      <div className="flex justify-center p-4  gap-4">
+        <AptitudeCard image={brain} title='Apptitude Test' para='Measures your logical, numerical, verbal, and analytical ablities' question={25} minute={30} style={` ${isDark ? 'bg-gray-600/40  border-purple-400/40' : 'bg-white border border-gray-200'}`} bgColor={`bg-blue-100`} progressColor={`bg-blue-600`} />
+        <AptitudeCard image={greenHeart} title='Interest Test' para='Help us understand your interests, preferences and personality' question={30} minute={30} style={` ${isDark ? 'bg-gray-600/40  border-purple-400/40' : 'bg-white border border-gray-200'}`} bgColor={`bg-green-100`} progressColor={`bg-green-600`} />
+      </div>
+      <div>
+        <p>You can't pause the assessment. Please ensure you have enough time.</p>
+      </div>
+      
+    </section>
   )
 }
 
