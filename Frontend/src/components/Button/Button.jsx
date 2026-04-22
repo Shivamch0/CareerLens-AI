dfe26ca649ea0a9458687bff120741261d0af3b9
Shivam <choudharyshivam0508@gmail.com>
2026-04-22 11:11:28 +0530

    Apptitude test completed

--- Frontend/src/components/Button/Button.jsx
+++ Frontend/src/components/Button/Button.jsx
@@ -1,13 +1,13 @@
 
-function Button({title , className , onClick , type}) {
+function Button({title , className , onClick , type , icon}) {
 
   return (
     <button 
-    className={`cursor-pointer font-bold rounded-2xl py-3 px-6 text-base sm:text-lg  ${className ? className : 'bg-blue-500 hover:bg-blue-800 '} transition-colors duration-300`} 
+    className={`cursor-pointer font-bold rounded-2xl  px-6 text-base sm:text-lg  ${className ? className : 'bg-blue-500 hover:bg-blue-800 py-3 '} transition-colors duration-300`} 
     onClick={onClick} 
     type={type}
     >
-      {title}
+      {title} {icon}
     </button>
   )
 }
--- Frontend/src/components/Card/Test.jsx
+++ Frontend/src/components/Card/Test.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
+
+// Components Imports
 import QuestionCard from "./QuestionCard";
+import Button from "../Button/Button"
+
+// Others Imports
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Test = () => {
   const [currentQ, setCurrentQ] = useState(7);
@@ -21,9 +28,13 @@ const Test = () => {
   }
 
   return (
-    <section className="flex-1 p-6">
-      {/* Header */}
-      <div className="flex justify-between items-center mb-6">
+    <section className="flex-1 px-6">
+
+      <div className="mb-1 p-4 bg-blue-100 rounded-xl flex  gap-2">
+        <p className="text-blue-500 font-semibold">💡Tips:</p>{tips.text}
+      </div>
+     <div>
+       <div className="flex justify-between items-center mb-6">
         <div>
           <p className="text-sm text-gray-500">Question {currentQ}/25</p>
         </div>
@@ -35,17 +46,20 @@ const Test = () => {
         <p className="cursor-pointer">Report 🚩</p>
       </div>
 
-      {/* Question Card */}
       <QuestionCard
         data={questionData}
         selected={selected}
         setSelected={setSelected}
       />
 
-      {/* Tips */}
-      <div className="mt-6 p-4 bg-gray-100 rounded-xl flex  gap-2">
-        <p className="text-blue-500 font-semibold">💡Tips:</p>{tips.text}
+      <div className="flex justify-between px-4 mt-2">
+        <Button title='Previous' className={`py-2 shadow shadow-black/30 font-semibold flex items-center gap-3 flex-row-reverse`} icon={<FaLongArrowAltLeft />} />
+        <Button title='Next'  className={`py-2 bg-blue-400 text-white font-semibold flex items-center gap-3`} icon={<FaLongArrowAltRight />}/>
       </div>
+     </div>
+
+      {/* Tips */}
+      
     </section>
   );
 };
