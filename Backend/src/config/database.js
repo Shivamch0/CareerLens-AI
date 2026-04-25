f57931ec3274f075bb9084a7123a837e084fe498
Shivam <choudharyshivam0508@gmail.com>
2026-04-25 12:15:45 +0530

    DNS setup completed

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import dns from "dns"
 import { DB_NAME } from '../constant.js';
+
+dns.setServers(["1.1.1.1" , "8.8.8.8"])
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.DB_URL}`)
+        const connection = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
         console.log("Database Connected successfully...")
     } catch (error) {
         console.log(`Database connection failed ${error}`)
