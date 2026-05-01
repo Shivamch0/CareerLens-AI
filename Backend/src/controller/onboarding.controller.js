e53b24cc433ce7562e8f540eee0609b32da7056c
Shivam <choudharyshivam0508@gmail.com>
2026-05-01 16:24:31 +0530

    feat: implement onboarding controller methods for user journey and interests

--- Backend/src/controller/onboarding.controller.js
+++ Backend/src/controller/onboarding.controller.js
@@ -1,2 +1,24 @@
 import { User } from '../models/user.model.js'
 
+// Utils Imports
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js'
+
+const saveJourney = asyncHandler (async (req , res) => {
+
+});
+
+const saveInterests = asyncHandler ( async (req , res) => {
+
+});
+
+const completeOnboarding = asyncHandler ( async ( req , res) => {
+
+});
+
+const getOnboardingStatus = asyncHandler (async (req , res) => {
+
+});
+
+export { saveJourney , saveInterests , completeOnboarding , getOnboardingStatus}
