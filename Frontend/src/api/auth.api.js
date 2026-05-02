88daa35cc0612c40978e46265eb96d0291ec04c9
Shivam <choudharyshivam0508@gmail.com>
2026-05-02 10:44:09 +0530

    fix: add missing exports for logoutUser, refreshToken, currentUser, changePassword, and changeDetails functions in auth API

--- Frontend/src/api/auth.api.js
+++ Frontend/src/api/auth.api.js
@@ -11,12 +11,28 @@ const loginUser = async (data) => {
 }
 
 const logoutUser = async (data) => {
-    const res = await api.post("/users/logout" , data)
-    return res.data
+    const res = await api.post("/users/logout" , data);
+    return res.data;
 }
 
 const refreshToken = async () => {
-    const res = await api.post("/users/refresh-token" , )
+    const res = await api.post("/users/refresh-token");
+    return res.data;
 }
 
-export {registerUser , loginUser}
+const currentUser = async () => {
+    const res = await api.get('/users/current-user');
+    return res.data;
+}
+
+const changePassword = async (data) => {
+    const res = await api.put("/users/change-password" , data);
+    return res.data;
+}
+
+const changeDetails = async (data) => {
+    const res = await api.put("/users/update-details" , data)
+    return res.data
+}
+
+export {registerUser , loginUser , logoutUser , refreshToken , currentUser , changeDetails , changePassword}
