c462a5abedc317a2bf9807cae8701714afebb280
Shivam <choudharyshivam0508@gmail.com>
2026-05-05 21:09:35 +0530

    feat: improve error handling in Gemini API integration

--- Backend/src/gemini api/gemini.js
+++ Backend/src/gemini api/gemini.js
@@ -1,4 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ApiError } from "../utils/ApiError.js";
+
+if(process.env.GEMINI_API_KEY){
+    throw new ApiError(400 , "Gemini API is not defined")
+}
 
 const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
@@ -10,9 +15,14 @@ export const generateResponse = async (prompt) => {
    try {
      const result = await model.generateContent(prompt)
      const response = await result.response;
-     return response.text();
+     const text = response.text();
+     if(!text){
+        throw new ApiError(400 , "Empty response from API...")
+     }
+
+     return text;
    } catch (error) {
-    console.log(`AI Error ${error}`)
+    console.log("AI Error" , error.message || error)
     throw error;
    }
 }
