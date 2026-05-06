e39c8ea39077b0bcd9189d9b7deeca01ae7d9528
Shivam <choudharyshivam0508@gmail.com>
2026-05-06 18:25:55 +0530

    fix: correct router variable name and ensure proper route definitions for tests

--- Backend/src/routes/test.routes.js
+++ Backend/src/routes/test.routes.js
@@ -2,8 +2,16 @@ import express from 'express';
 import { generateAptitudeQuestions , generateInterestQuestions , submitAptitudeTest , submitInterestTest , getCareerRecommendations } from '../controller/test.controller.js';
 import { verifyJWT } from "../middleware/auth.middleware.js"
 
-const route = express.Router();
+const router = express.Router();
 
+router.route('/interest-questions').get(verifyJWT , generateInterestQuestions);
 
-export default route;
+router.route('/aptitude-questions').get(verifyJWT , generateAptitudeQuestions);
 
+router.route('/interest-submit').post(verifyJWT , submitInterestTest);
+
+router.route('/aptitude-submit').post(verifyJWT , submitAptitudeTest);
+
+router.route('/career-recommendations').get(verifyJWT , getCareerRecommendations);
+
+export default router;
